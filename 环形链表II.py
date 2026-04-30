6afe1c4f7ea29a7c5c9a2fb88ab4e892eaee482d
chenxingyong <2103095120@qq.com>
2026-04-30 17:56:49 +0800

    环形链表 II

---
+++ 环形链表II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while slow is not head:
+                    slow = slow.next
+                    head = head.next
+                return slow
+        return None
