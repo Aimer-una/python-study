c846dffcadbc6a567c4fcfb9c9d4447aed36516d
chenxingyong <2103095120@qq.com>
2026-05-05 20:36:59 +0800

    两两交换链表中的节点

---
+++ 两两交换链表中的节点.py
@@ -0,0 +1,18 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        node0 = dummy
+        node1 = head
+        while node1 and node1.next:
+            node2 = node1.next
+            node3 = node2.next
+
+            node0.next = node2
+            node2.next = node1
+            node1.next = node3
+
+            node0 = node1
+            node1 = node3
+
+        return dummy.next
