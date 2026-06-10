86600572fe1f19e2d0da904327939c8215a01667
chenxingyong <2103095120@qq.com>
2026-06-10 16:03:27 +0800

    feat(scraper): 实现电影详情爬取功能
    - 添加了get_movie_info函数来获取电影详细信息
    - 实现了电影名称、类型、上映时间、评分等字段的提取
    - 使用xpath解析电影详情页面的各个元素
    - 添加了数据清洗和异常处理逻辑
    - 暂时注释掉CSV保存功能，改为直接打印结果以便调试

--- 网络机器人/案例电影榜单-1.py
+++ 网络机器人/案例电影榜单-1.py
@@ -4,9 +4,35 @@ from lxml import html
 # 常量
 BASE_URL = 'https://ssr1.scrape.center'
 
-
+# 获取电影详情
 def get_movie_info(movie_url):
-    pass
+    movie_response = requests.get(movie_url,timeout=10)
+    movie_doc = html.fromstring(movie_response.text)
+
+    # 电影名称
+    movie_names = movie_doc.xpath("//h2[@class='m-b-sm']/text()")
+    # 电影类型
+    movie_types = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[2]/div[1]/button/span/text()")
+    # 电影上映时间
+    movie_dates = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[2]/div[3]/span/text()")
+    # 电影评分
+    movie_scores = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[3]/p[1]/text()")
+    # 上线地区
+    movie_areas = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[2]/div[2]/span[1]/text()")
+    # 电影时长
+    movie_times = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[2]/div[2]/span[3]/text()")
+    # 电影简介
+    movie_intros = movie_doc.xpath("//*[@id='detail']/div[1]/div/div/div[1]/div/div[2]/div[4]/p/text()")
+    movie_info = {
+        "名称": movie_names[0].strip() if movie_names else '',
+        "类型": ",".join(movie_types) if movie_types else '',
+        "上映时间": movie_dates[0].strip() if movie_dates else '',
+        "评分": movie_scores[0].strip() if movie_scores else '',
+        "地区": ",".join(movie_areas) if movie_areas else '',
+        "时长": movie_times[0].strip() if movie_times else '',
+        "简介": movie_intros[0].strip() if movie_intros else ''
+    }
+    print(movie_info)
 
 
 
@@ -46,12 +72,12 @@ def main():
             urls = movie.xpath("./div/div[@class = 'el-col el-col-24 el-col-xs-8 el-col-sm-6 el-col-md-4']/a/@href")
             if urls:
                 movie_url = BASE_URL + urls[0]
-                print(movie_url)
                 # 发起请求，获取电影详情数据
                 movie_info = get_movie_info(movie_url)
                 all_movies.append(movie_info)
 
         # 保存到csv文件
-        save_all_movies(all_movies)
+        #save_all_movies(all_movies)
+        print(all_movies)
 if __name__ == '__main__':
     main()
